68d8886e16b30ea0fbc345c9f8bb661091527a2f
oyevstafyev <oievstafiev@jahia.com>
2015-09-23 11:31:07 +0200

    QA-7628 - Fix multi-threading issues

--- api/src/main/java/org/jahia/dm/DocumentManagement.java
+++ api/src/main/java/org/jahia/dm/DocumentManagement.java
@@ -88,7 +88,7 @@ import org.slf4j.LoggerFactory;
  */
 public class DocumentManagement implements BundleContextAware {
 
-    private static DocumentManagement instance;
+    private static volatile DocumentManagement instance;
     private static final Logger logger = LoggerFactory.getLogger(DocumentManagement.class);
 
     private BundleContext bundleContext;
