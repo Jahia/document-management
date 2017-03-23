c36ca4ed8cf2a447663d6b2a818cf6cddb17b7dc
oyevstafyev <oievstafiev@jahia.com>
2017-03-23 11:00:19 +0100

    [QA-9192] Update DX copyright to 2017

--- viewer/web/src/main/resources/javascript/jahia.swfview.js
+++ viewer/web/src/main/resources/javascript/jahia.swfview.js
@@ -4,33 +4,33 @@
  * @url		http://www.jahia.org/
  * @author	Sergiy Shyrkov <sergiy.shyrkov@jahia.com>
  * @version	1.1
- * Copyright (C) 2002-2015 Jahia Solutions Group SA. All rights reserved.
+ * Copyright (C) 2002-2017 Jahia Solutions Group SA. All rights reserved.
  */
 ;(function($) {
-	$.fn.docViewer = function(options) {
-		var player = (typeof contextJsParameters != 'undefined' && typeof contextJsParameters.contextPath != 'undefined' ? contextJsParameters.contextPath : '') + '/modules/document-viewer/javascript/flexpaper/FlexPaperViewer.swf';
-		var allOpts = $.extend({}, $.fn.docViewer.defaults, options);
-		
-		return this.each(function() {
-			var opts = allOpts;
-			if (typeof allOpts.SwfFile == 'undefined' && typeof $(this).attr('rel') != 'undefined') {
+    $.fn.docViewer = function(options) {
+        var player = (typeof contextJsParameters != 'undefined' && typeof contextJsParameters.contextPath != 'undefined' ? contextJsParameters.contextPath : '') + '/modules/document-viewer/javascript/flexpaper/FlexPaperViewer.swf';
+        var allOpts = $.extend({}, $.fn.docViewer.defaults, options);
+
+        return this.each(function() {
+            var opts = allOpts;
+            if (typeof allOpts.SwfFile == 'undefined' && typeof $(this).attr('rel') != 'undefined') {
                 swfFile = $(this).attr('rel');
                 swfFile = swfFile.replace("'","%27");
                 opts = $.extend({}, allOpts, {SwfFile : swfFile});
-			}
-			if (typeof $(this).data("docViewer") != 'undefined') {
-				$(this).data("docViewer").getApi().loadSwf(opts.SwfFile);
-			} else {
-				$(this).data("docViewer", flashembed(this, {
-	            	src: player,
-	            	version: [10, 0],
-	            	wmode: 'transparent'
-	            }, opts));
-			}
-		});
-	}
+            }
+            if (typeof $(this).data("docViewer") != 'undefined') {
+                $(this).data("docViewer").getApi().loadSwf(opts.SwfFile);
+            } else {
+                $(this).data("docViewer", flashembed(this, {
+                    src: player,
+                    version: [10, 0],
+                    wmode: 'transparent'
+                }, opts));
+            }
+        });
+    }
 
-	$.fn.docViewer.defaults = {
+    $.fn.docViewer.defaults = {
             Scale : 0.6,
             ZoomTransition : 'easeOut',
             ZoomTime : 0.5,
@@ -44,13 +44,13 @@
             SearchMatchAll : false,
             InitViewMode : 'Portrait',
             PrintPaperAsBitmap : false,
-             
+
             ViewModeToolsVisible : true,
             ZoomToolsVisible : true,
             NavToolsVisible : true,
             CursorToolsVisible : true,
             SearchToolsVisible : true,
-                
+
             localeChain: 'en_US'
     }
 })(jQuery);
